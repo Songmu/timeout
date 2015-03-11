111f49a782c9c5dee2bb6398b887e3b2f8712ca3
Songmu <y.songmu@gmail.com>
2015-03-12 01:43:46 +0900

    fix test order

--- timeout_test.go
+++ timeout_test.go
@@ -7,53 +7,6 @@ import (
 	"testing"
 )
 
-func TestParseDuration(t *testing.T) {
-	v, err := parseDuration("55s")
-	if err != nil {
-		t.Errorf("something wrong")
-	}
-	if v != 55 {
-		t.Errorf("parse failed!")
-	}
-
-	v, err = parseDuration("55")
-	if err != nil {
-		t.Errorf("something wrong")
-	}
-	if v != 55 {
-		t.Errorf("parse failed!")
-	}
-
-	v, err = parseDuration("10m")
-	if err != nil {
-		t.Errorf("something wrong")
-	}
-	if v != 600 {
-		t.Errorf("parse failed!")
-	}
-
-	v, err = parseDuration("1h")
-	if err != nil {
-		t.Errorf("something wrong")
-	}
-	if v != 3600 {
-		t.Errorf("parse failed!")
-	}
-
-	v, err = parseDuration("1d")
-	if err != nil {
-		t.Errorf("something wrong")
-	}
-	if v != 86400 {
-		t.Errorf("parse failed!")
-	}
-
-	_, err = parseDuration("1w")
-	if err == nil {
-		t.Errorf("something wrong")
-	}
-}
-
 func TestRun(t *testing.T) {
 	tio := &Timeout{
 		Cmd: exec.Command("/bin/sh", "-c", "echo 1"),
@@ -129,3 +82,51 @@ func TestCommandNotFound(t *testing.T) {
 		t.Errorf("something wrong")
 	}
 }
+
+func TestParseDuration(t *testing.T) {
+	v, err := parseDuration("55s")
+	if err != nil {
+		t.Errorf("something wrong")
+	}
+	if v != 55 {
+		t.Errorf("parse failed!")
+	}
+
+	v, err = parseDuration("55")
+	if err != nil {
+		t.Errorf("something wrong")
+	}
+	if v != 55 {
+		t.Errorf("parse failed!")
+	}
+
+	v, err = parseDuration("10m")
+	if err != nil {
+		t.Errorf("something wrong")
+	}
+	if v != 600 {
+		t.Errorf("parse failed!")
+	}
+
+	v, err = parseDuration("1h")
+	if err != nil {
+		t.Errorf("something wrong")
+	}
+	if v != 3600 {
+		t.Errorf("parse failed!")
+	}
+
+	v, err = parseDuration("1d")
+	if err != nil {
+		t.Errorf("something wrong")
+	}
+	if v != 86400 {
+		t.Errorf("parse failed!")
+	}
+
+	_, err = parseDuration("1w")
+	if err == nil {
+		t.Errorf("something wrong")
+	}
+}
+
