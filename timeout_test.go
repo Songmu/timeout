c40bf49a5c1935bd05f180140fd16c846147e661
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2017-03-22 15:14:06 +0900

    skip some tests on Windows

--- timeout_test.go
+++ timeout_test.go
@@ -3,15 +3,29 @@ package timeout
 import (
 	"os"
 	"os/exec"
+	"runtime"
+	"strings"
 	"syscall"
 	"testing"
 	"time"
 )
 
+var (
+	shellcmd  = "/bin/sh"
+	shellflag = "-c"
+)
+
+func init() {
+	if runtime.GOOS == "windows" {
+		shellcmd = "cmd"
+		shellflag = "/c"
+	}
+}
+
 func TestRunSimple(t *testing.T) {
 	tio := &Timeout{
 		Duration: time.Duration(0.1 * float64(time.Second)),
-		Cmd:      exec.Command("/bin/sh", "-c", "echo 1"),
+		Cmd:      exec.Command(shellcmd, shellflag, "echo 1"),
 	}
 	exit := tio.RunSimple(false)
 
@@ -23,11 +37,11 @@ func TestRunSimple(t *testing.T) {
 func TestRun(t *testing.T) {
 	tio := &Timeout{
 		Duration: 10 * time.Second,
-		Cmd:      exec.Command("/bin/sh", "-c", "echo 1"),
+		Cmd:      exec.Command(shellcmd, shellflag, "echo 1"),
 	}
 	_, stdout, stderr, err := tio.Run()
 
-	if stdout != "1\n" {
+	if strings.TrimSpace(stdout) != "1" {
 		t.Errorf("something wrong")
 	}
 
@@ -36,13 +50,13 @@ func TestRun(t *testing.T) {
 	}
 
 	if err != nil {
-		t.Errorf("something wrong")
+		t.Errorf("something wrong: %v", err)
 	}
 }
 
 func TestRunTimeout(t *testing.T) {
 	tio := &Timeout{
-		Cmd:      exec.Command("/bin/sh", "-c", "sleep 3"),
+		Cmd:      exec.Command(shellcmd, shellflag, "sleep 3"),
 		Duration: 1 * time.Second,
 		Signal:   os.Interrupt,
 	}
@@ -54,6 +68,9 @@ func TestRunTimeout(t *testing.T) {
 }
 
 func TestPreserveStatus(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.Skipf("skip test on Windows")
+	}
 	tio := &Timeout{
 		Cmd:      exec.Command("perl", "test/exit_with_23.pl"),
 		Duration: 1 * time.Second,
@@ -61,7 +78,7 @@ func TestPreserveStatus(t *testing.T) {
 
 	exit := tio.RunSimple(true)
 	if exit != 23 {
-		t.Errorf("something wrong")
+		t.Errorf("something wrong: %v", exit)
 	}
 }
 
@@ -75,7 +92,7 @@ func TestKillAfter(t *testing.T) {
 	exit := tio.RunSimple(true)
 
 	if exit != 137 {
-		t.Errorf("something wrong")
+		t.Errorf("something wrong: %v", exit)
 	}
 }
 
@@ -89,11 +106,15 @@ func TestKillAfterNotKilled(t *testing.T) {
 	exit := tio.RunSimple(true)
 
 	if exit != 0 {
-		t.Errorf("something wrong")
+		t.Errorf("something wrong: %v", exit)
 	}
 }
 
 func TestCommandCannotBeInvoked(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		// TODO cmd return 125 for this case
+		t.Skipf("skip test on Windows")
+	}
 	tio := &Timeout{
 		Cmd:      exec.Command("test/dummy"),
 		Duration: 1 * time.Second,
@@ -101,11 +122,15 @@ func TestCommandCannotBeInvoked(t *testing.T) {
 	exit := tio.RunSimple(false)
 
 	if exit != 126 {
-		t.Errorf("something wrong")
+		t.Errorf("something wrong: %v", exit)
 	}
 }
 
 func TestCommandNotFound(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		// TODO cmd return 125 for this case
+		t.Skipf("skip test on Windows")
+	}
 	tio := &Timeout{
 		Cmd:      exec.Command("test/ignore_sigterm.pl-xxxxxxxxxxxxxxxxxxxxx"),
 		Duration: 1 * time.Second,
@@ -113,6 +138,6 @@ func TestCommandNotFound(t *testing.T) {
 	exit := tio.RunSimple(false)
 
 	if exit != 127 {
-		t.Errorf("something wrong")
+		t.Errorf("something wrong: %v", exit)
 	}
 }
